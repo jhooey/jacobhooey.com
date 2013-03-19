36ed26ad635177ecc7e0a54627fc987972e39de9
jhooey <jacob@pompouspanda.com>
2013-03-18 22:53:18 -0600

    finished code cleanup - closes #7

--- templates/footer.php
+++ templates/footer.php
@@ -22,8 +22,7 @@
         <a href="http://ca.linkedin.com/pub/jacob-hooey/36/130/a48">LINKED<span class="green">IN</span></a> |
         <a href="https://github.com/jhooey">GIT<span class="green">HUB</span></a> |
         <a href="http://facebook.com/jhooey">FACE<span class="green">BOOK</span></a> |
-        <a href="http://twitter.com/#!/jacobhooey">TWI<span class="green">TTER</span></a><!-- |
-        <a href='#'>R<span class="green">SS</span></a>-->
+        <a href="http://twitter.com/#!/jacobhooey">TWI<span class="green">TTER</span></a>
     </p>
 </div>
 
