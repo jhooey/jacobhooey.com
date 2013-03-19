17a772859f6267f4f9c41607f5cb86e3840727da
jhooey <jacob@pompouspanda.com>
2013-03-18 22:48:41 -0600

    removed indent from the facebook button
    removed the padding attribute fromsocial.php

--- templates/social.php
+++ templates/social.php
@@ -10,7 +10,7 @@
 
 <div class="social-network">
   <div style="float:right;padding:0px;margin:0px;width:60px;">
-    <div class="fb-like" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true" style="padding-bottom:5px;padding-left:16px;"></div>
+    <div class="fb-like" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true" style="padding-bottom:5px;"></div>
     <div class="g-plusone" data-size="medium"></div>
     <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.jacobhooey.com" data-via="jacobhooey"  data-count="none">Tweet</a>
     <script src="http://platform.linkedin.com/in.js" type="text/javascript"></script>
