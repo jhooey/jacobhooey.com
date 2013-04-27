05ae8593ba92b0791b0f37105ff83dc9d173e7d9
Jacob Hooey <jacob@jacobhooey.com>
2013-04-27 01:00:16 -0400

    added codemirror files

--- admin/templates/include/header.php
+++ admin/templates/include/header.php
@@ -3,6 +3,18 @@
   <head>
     <title><?php echo htmlspecialchars( $results['pageTitle'] )?></title>
     <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" href="../js/codemirror/codemirror.css">
+    <link rel="stylesheet" href="../js/codemirror/docs.css">
+    
+    <script src="../js/codemirror/codemirror.js"></script>
+    <script src="../js/codemirror/xml.js"></script>
+    <script src="../js/codemirror/javascript.js"></script>
+    <script src="../js/codemirror/css.js"></script>
+    <script src="../js/codemirror/vbscript.js"></script>
+    <script src="../js/codemirror/htmlmixed.js"></script>
+
+    <style>.CodeMirror {border-top: 1px solid black; border-bottom: 1px solid black;}</style>
+    
   </head>
   <body>
     <div id="container">
